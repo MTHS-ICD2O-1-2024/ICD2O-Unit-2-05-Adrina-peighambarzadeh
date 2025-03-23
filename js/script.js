cbad6be47bb79759b530f36d63a340ee14f2ddae
default <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-03-23 00:18:20 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Adrina. peighambarzadeh All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Adrina. peighambarzadeh
+// Created on: mar 2025
 // This file contains the JS functions for index.html
 
-/**
- * Displays an alert with the message "Hello, World!"
- */
-function myButtonClicked() {
-  alert("Hello, World!")
+"use strict"
+
+function calculateTakeHomePay() {
+  // input
+  const hoursWorked = parseFloat(document.getElementById('hours-worked').value);
+  const hourlyWage = parseFloat(document.getElementById('hourly-wage').value);
+  
+  
+  // process
+  const grossPay = hoursWorked * hourlyWgae;
+  const taxAmount = grossPay * 0.18;
+  const takeHomeSalary = grossPay - taxAmount;
+  
+  // output
+  document.getElementById('pay').innerHTML = `Your pay will be: $ ${takeHomeSalary.toFixed(2)}`
+document.getElementById('pay').innerHTML = 'Your pay will be: $' + takeHomeSalary.toFixed(2)
 }
+
